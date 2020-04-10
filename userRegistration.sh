683bf656fb27e7597ad05464b9304c23b8348c5f
Bhavesh Kadam <bkadam357@gmail.com>
2020-04-10 19:58:38 +0530

    ADDED: code to validate first name

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,2 +1,15 @@
 #!/bin/bash -x
+
+shopt -s extglob
+#variable
+
 Echo "Welcome to user registration"
+echo "Enter your first name"
+read first
+pat="^[A-Z][a-z]{2,}$"
+if [[ $first =~ $pat ]]
+then
+	echo "Proper Format"
+else
+	echo "Enter Proper Name"
+fi
