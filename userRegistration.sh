3c6ba7c92956b7a9adcce5104170c43a9d36be66
bhavesh357 <bkadam357@gmail.com>
2020-04-10 16:31:14 +0000

    ADDED: function to validate email

--- userRegistration.sh
+++ userRegistration.sh
@@ -14,10 +14,25 @@ function nameValidation() {
 		echo "Enter Proper Name"
 	fi
 }
+
+function emailValidation() {
+  email=$1
+  pat="[a-z]{1}([A-Za-z0-9]{1,}|[A-Za-z0-9]{1,}[-.]{1}[A-Za-z0-9]{1,})[A-Za-z0-9]{1}@[a-z0-9]{1,}.(com|net).?(au|com)?"
+  if [[ $email =~ $pat ]]
+  then
+    echo good email
+  else
+    echo enter proper email
+  fi
+}
+
 Echo "Welcome to user registration"
-echo "Enter your first name"
-read first
-nameValidation $first
-echo "Enter your first name"
-read last
-nameValidation $last
+#echo "Enter your first name"
+#read first
+#nameValidation $first
+#echo "Enter your last name"
+#read last
+#nameValidation $last
+echo "Enter your emailId"
+read email
+emailValidation $email
