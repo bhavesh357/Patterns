8c0496eb067fdb2e0a8bed02bee264e63e777021
Bhavesh Kadam <bkadam357@gmail.com>
2020-04-14 15:15:30 +0530

    MODIFIED: code to increase readibility

--- userRegistration.sh
+++ userRegistration.sh
@@ -2,65 +2,37 @@
 
 shopt -s extglob
 #variable
-par=""
 
+
+passwordPat="^(?=.*[A-Z])(?=.*[0-9])(?=.*[A-Za-z0-9@#!$%^&*()_-]{8,})[A-Za-z0-9]{1,}[@#!$%^&*()_-][A-Za-z0-9]{1,}$"
+mobilePat="[0-9]{2} [0-9]{10}"
+emailPat="[\w\d]{1,}[.\-#!]?[\w\d]{1,}@[\w\d]{1,}.(com|co|net).?([a-z]{2})?"
+namePat="^[A-Z][a-z]{2,}$"
+		
 function nameValidation() {
-	name=$1
-	pat="^[A-Z][a-z]{2,}$"
-	if [[ $name =~ $pat ]]
+	word=$1
+	pat=$2
+	if [[ $word =~ $pat ]]
 	then
 		echo "Proper Format"
 	else
-		echo "Enter Proper Name"
-	fi
-}
-
-function emailValidation() {
-	email=$1
-	pat="[\w\d]{1,}[.\-#!]?[\w\d]{1,}@[\w\d]{1,}.(com|co|net).?([a-z]{2})?"
-	if [[ $email =~ $pat ]]
-	then
-		echo good email
-	else
-		echo enter proper email
-	fi
-}
-
-function mobileValidation() {
-	mobile=$1
-	pat="[0-9]{2} [0-9]{10}"
-	if [[ $mobile =~ $pat ]]
-	then
-		echo good mobile
-	else
-		echo enter proper mobile
-	fi
-}
-
-function passwordValidation() {
-	password=$1
-	pat="^(?=.*[A-Z])(?=.*[0-9])(?=.*[A-Za-z0-9]{8,})[A-Za-z0-9]{1,}[@#!$%^&*()_-][A-Za-z0-9]{1,}$"
-	if [[ $password =~ $pat ]]
-	then
-		echo good password
-	else
-		echo enter proper password
+		echo "Not Proper Format"
 	fi
 }
 
 Echo "Welcome to user registration"
 echo "Enter your first name"
 read first
-nameValidation $first
+nameValidation $first $namePat
 echo "Enter your last name"
 read last
-nameValidation $last
+nameValidation $last $namePat
 echo "Enter your emailId"
 read email
-emailValidation $email
+emailValidation $email $emailPat
 echo "Enter your mobile no"
 read mobile
-mobileValidation $mobile
+mobileValidation $mobile $mobilePat
 echo "Enter your password"
 read password
-passwordValidation $password
+passwordValidation $password $passwordPat
